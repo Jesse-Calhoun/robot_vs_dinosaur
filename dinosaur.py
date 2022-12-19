ea3441d0db03467b1ddfd9ecebf1a070776bcb2d
Jesse Calhoun <jessecalhoun@Jesses-MacBook-Air.local>
2022-12-19 12:30:15 -0500

    Inside of the __init__() for Dinosaur inside of the dinosaur.py file, I added to the properies: name and attack_power. I created instance variables within the __init__() function: self.name, self. attack_power which are both assigned to the name and attack_power properties. I also had one more instance variable for health which I appled the integer 100 to. I then created the attack method wihin the Dinosaur class and set it up with a self property and a robot property. After the properties were set up I set the robot property assigned to robot - self.attack_power to take away the attack power from the given robots health.

--- dinosaur.py
+++ dinosaur.py
@@ -1,5 +1,10 @@
 
 
 class Dinosaur:
-    def __init__(self) -> None:
-        pass
+    def __init__(self, name, attack_power):
+        self.name = name
+        self.attack_power = attack_power
+        self.health = 100
+    
+    def attack(self, robot):
+        robot -= self.attack_power
