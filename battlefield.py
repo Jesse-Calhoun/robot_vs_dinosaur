4ffa8d86ba3a95bf6e889fd18224ccee6b8423a5
Jesse Calhoun <jessecalhoun@Jesses-MacBook-Air.local>
2022-12-19 14:22:16 -0500

    I've imported the Robot class from the robot.py file, the Weapon class from the weapon.py file, and the Dinosaur class from the dinosaur.py file into the battlefield.py file. I created the __init__ () function with the properties robot: Robot (which means the robot name given will be run through the Robot class.) and the dinosaur: Dinosaur property(dinosaur object given will be run through Dinosaur class.)

--- battlefield.py
+++ battlefield.py
@@ -1,5 +1,10 @@
-
+from robot import Robot
+from weapon import Weapon
+from dinosaur import Dinosaur
 
 class Battlefield:
-    def __init__(self) -> None:
-        pass
+    def __init__(self, robot: Robot, dinosaur: Dinosaur):
+        self.robot = robot
+        self.dinosaur = dinosaur
+    
+    def 
