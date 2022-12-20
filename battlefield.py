2129fce9946217799ff678691965b010a4b2a4e1
Jesse Calhoun <jessecalhoun@Jesses-MacBook-Air.local>
2022-12-20 17:58:18 -0500

    Within the battlefield.py file, I imported the FLeet class from the fleet.py file, and the Herd class from the herd.py file. I commented out the original __inti__ set up instance variables for 3 Dinosaur method instances and 3 Robot method instances. created lists for both the dinosaurs, named dinos, and the robots, name fleet_of_robots. I set class variables: self.herd assigned to the Herd('Jurassic Pride', dinos) and self.fleet assigned Fleet('The Tin Men', fleet_of_robots. Line 33 I rewrote the battle_phase method by calling the combine_health functions for both the self. her and self.fleet(both holding their respective methods). I combine the herd power with the combine_power method on line 36. Then with a while loop determining whether both participant groups are alive, the fleet's power is set back to 0 on line 38, then combine_power method for the fleet on line 39, so as to pick new weapons before each attack and recombine the power but from 0 not the last run of fleet_power. When wrewriting thr diplay_winner phase just some variables needed to be changed within the strings being printed out. I also commented out previous battle_phase and display_winner functions, in order to revisit if needed.

--- battlefield.py
+++ battlefield.py
@@ -4,8 +4,18 @@ from fleet import Fleet
 from herd import Herd
 class Battlefield:
     def __init__(self):
-        self.robot = Robot('Bender')
-        self.dinosaur = Dinosaur('Rex', 15)
+        trex = Dinosaur('Rex', 17)
+        ptero = Dinosaur('Petrie', 15)
+        snork = Dinosaur('Dino', 13)
+        dinos = [trex, ptero, snork]
+        bender = Robot('Bender') 
+        wall_e = Robot('Wall-E')
+        t_800 = Robot('Arnold')
+        fleet_of_robots = [bender, wall_e, t_800]
+        self.herd = Herd('Jurassic Pride', dinos)
+        self.fleet = Fleet('The Tin Men', fleet_of_robots)
+        # self.robot = Robot('Bender')
+        # self.dinosaur = Dinosaur('Rex', 15)
     
     def run_game(self):
         self.display_welcome()
@@ -21,20 +31,39 @@ Let the battle begin!
 ''')
 
     def battle_phase(self):
-        while self.robot.is_alive() and self.dinosaur.is_alive():
-            self.robot.attack(self.dinosaur)
-            self.dinosaur.attack(self.robot)
+        self.fleet.combine_health()
+        self.herd.combine_health()
+        self.herd.combine_power()
+        while self.fleet.is_alive() and self.herd.is_alive():
+            self.fleet.fleet_power = 0
+            self.fleet.combine_power()
+            self.fleet.attack(self.herd)
+            self.herd.attack(self.fleet)
 
-
+    # def battle_phase(self):
+    #     while self.robot.is_alive() and self.dinosaur.is_alive():
+    #         self.robot.attack(self.dinosaur)
+    #         self.dinosaur.attack(self.robot)
 
     def display_winner(self):
-        if self.robot.health > 0:
+        if self.fleet.fleet_health > 0:
             print(f'''
-{self.robot.name} has defeated {self.dinosaur.name}!
-{self.robot.name} wins!
+{self.fleet.name} has defeated {self.herd.name}!
+{self.fleet.name} wins!
 ''')
-        elif self.dinosaur.health > 0:
+        elif self.herd.herd_health > 0:
             print(f'''
-{self.dinosaur.name} has defeated {self.robot.name}!
-{self.dinosaur.name} wins!
+{self.herd.name} has defeated {self.fleet.name}!
+{self.herd.name} wins!
 ''')
+#     def display_winner(self):
+#         if self.robot.health > 0:
+#             print(f'''
+# {self.robot.name} has defeated {self.dinosaur.name}!
+# {self.robot.name} wins!
+# ''')
+#         elif self.dinosaur.health > 0:
+#             print(f'''
+# {self.dinosaur.name} has defeated {self.robot.name}!
+# {self.dinosaur.name} wins!
+# ''')
