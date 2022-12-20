2545265e726e8010e41cbc7587130dc9f264cdaa
Jesse Calhoun <jessecalhoun@Jesses-MacBook-Air.local>
2022-12-20 17:41:33 -0500

    In erd. py, I created the __inti__() method with properties: self, name (needing to be a string), and herd (needing to be a list). INside the scope of the init instance variables: self.name assigned to 'name' property, self.herd_health = 0, self.herd_power = 0, and self.herd assigned to the property 'herd'(requiring a list). Line 10 I created the combine_health method that uses for loops, range and len()function to combine the health of the objects in the list. Similarly using the for loops, range and len() fucntion I created the combine_power() method to combine the attack power of the objects in the list. Line 18 I created the attack() method with parameter 'fleet' requiring a list input, that runs as a conditional if statement, if self.is_alive(), then herd will attack the fleet, subtracting the herd_power from the fleet.fleet_health and printing the attack to the console. On line 26 I created the is_alive() method to determine if the herd_health is above 0.

--- herd.py
+++ herd.py
@@ -1,5 +1,27 @@
 from dinosaur import Dinosaur
 
 class Herd:
-    def __init__(self):
-        pass
+    def __init__(self, name: str, herd: list):
+        self.name = name
+        self.herd_health = 0
+        self.herd_power = 0
+        self.herd = herd
+
+    def combine_health(self):
+        for i in range(len(self.herd)):
+            self.herd_health += self.herd[i].health
+    
+    def combine_power(self):
+        for i in range(len(self.herd)):
+            self.herd_power += self.herd[i].attack_power
+    
+    def attack(self, fleet: list):
+        if self.is_alive():
+            fleet.fleet_health -= self.herd_power
+            print(f'''
+            The herd of dinosaurs, named {self.name}, attacks the fleet, named {fleet.name} causing {self.herd_power} points of damage.
+            {fleet.name} has {fleet.fleet_health} health remaining.
+            ''')
+    
+    def is_alive(self):
+        return self.herd_health > 0
