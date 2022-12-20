8e3f6b5eea1e198e5e1311c4228e0ad25c74efce
Jesse Calhoun <jessecalhoun@Jesses-MacBook-Air.local>
2022-12-20 12:50:19 -0500

    In robot.py on line 25 I created the choose_weapon() method and had it return the user input previously on line 15, and place it inside the index for the self.active_weapon list, the string being return from the input being changed into an integer to be used in the indexing of the list, have it returned on line 26. On line 14 above the if coditional I call the slef.choose_weapon() method and assign it to the active_weapon instance variable to be used within the attack() function.

--- robot.py
+++ robot.py
@@ -2,17 +2,17 @@ from weapon import Weapon
 class Robot:
    
     def __init__(self, name:str):
-        excalibur = Weapon('Excalibur', 17)
+        excalibur = Weapon('Excalibur', 16)
         grass_cutter = Weapon('The Grass-Cutter', 14)
-        mjolnir = Weapon('Mjolnir', 25)
+        mjolnir = Weapon('Mjolnir', 23)
         
         self.name = name
         self.health = 100
         self.active_weapon = [excalibur, grass_cutter, mjolnir]
 
     def attack(self, dinosaur):
+        active_weapon = self.choose_weapon()
         if self.is_alive():
-            active_weapon = self.active_weapon[int(input(f'Which weapon would you like {self.name} to use? Tyoe 0 for Excalibur, 1 for The Grass-Cutter, or 2 for Mjolnir: '))]
             dinosaur.health -= active_weapon.attack_power
             print(f'''
 {self.name} attacked {dinosaur.name} with {active_weapon.name} causing {active_weapon.attack_power} damage!
@@ -22,3 +22,5 @@ class Robot:
     def is_alive(self):
         return self.health > 0
     
+    def choose_weapon(self):
+        return self.active_weapon[int(input(f'Which weapon would you like {self.name} to use? Tyoe 0 for Excalibur, 1 for The Grass-Cutter, or 2 for Mjolnir: '))]
